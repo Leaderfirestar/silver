b39a9fd2fe3cca51c3ca15bd8126377331bfb83e
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 11:31:00 -0500

    created base silver ingot item

--- src/main/java/com/leaderfirestar/silver/setup/ModItems.java
+++ src/main/java/com/leaderfirestar/silver/setup/ModItems.java
@@ -1,2 +1,13 @@
-package com.leaderfirestar.silver.setup;public class ModItems {
+package com.leaderfirestar.silver.setup;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+
+public class ModItems {
+    public static final RegistryObject<Item> SILVER_INGOT = Registration.Items.register("silver_ingot", () ->
+            new Item(new Item.Properties().tab(ItemGroup.TAB_MATERIALS)));
+    static void register(){
+
+    }
 }
