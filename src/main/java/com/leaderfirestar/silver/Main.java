02885917fdb3d1ae5804cfc442ea262fbe02e738
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 11:32:00 -0500

    Created mod_id string. Called register function to register other classes

--- src/main/java/com/leaderfirestar/silver/Main.java
+++ src/main/java/com/leaderfirestar/silver/Main.java
@@ -1,5 +1,6 @@
 package com.leaderfirestar.silver;
 
+import com.leaderfirestar.silver.setup.Registration;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -19,13 +20,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("silver")
+@Mod(Main.MOD_ID)
 public class Main
 {
+    public static final String MOD_ID = "silver";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
     public Main() {
+        Registration.register();
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
