02659e4cbac7ea695456f44df0564cfde7e05f4d
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 19:48:39 -0500

    Added provider for blockstate

--- src/main/java/com/leaderfirestar/silver/data/DataGenerators.java
+++ src/main/java/com/leaderfirestar/silver/data/DataGenerators.java
@@ -1,2 +1,22 @@
-package com.leaderfirestar.silver.data;public class DataGenerators {
+package com.leaderfirestar.silver.data;
+
+import com.leaderfirestar.silver.Main;
+import com.leaderfirestar.silver.data.client.ModBlockState;
+import com.leaderfirestar.silver.data.client.ModItemModels;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = Main.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    private DataGenerators(){}
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent e){
+        DataGenerator gen = e.getGenerator();
+        ExistingFileHelper existingFileHelper = e.getExistingFileHelper();
+        gen.addProvider(new ModItemModels(gen, existingFileHelper));
+        gen.addProvider(new ModBlockState(gen, existingFileHelper));
+    }
 }
