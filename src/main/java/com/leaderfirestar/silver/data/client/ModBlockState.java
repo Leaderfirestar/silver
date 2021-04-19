2f507a0c9c7d9bf921d1d4a11f28aec4ef19c2d2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-18 19:49:44 -0500

    Registered models and blockstates

--- src/main/java/com/leaderfirestar/silver/data/client/ModBlockState.java
+++ src/main/java/com/leaderfirestar/silver/data/client/ModBlockState.java
@@ -1,2 +1,18 @@
-package com.leaderfirestar.silver.data.client;public class ModBlockState {
+package com.leaderfirestar.silver.data.client;
+
+import com.leaderfirestar.silver.Main;
+import com.leaderfirestar.silver.setup.ModBlocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModBlockState extends BlockStateProvider {
+    public ModBlockState(DataGenerator gen, ExistingFileHelper existingFileHelper){
+        super(gen, Main.MOD_ID, existingFileHelper);
+    }
+    @Override
+    protected void registerStatesAndModels(){
+        simpleBlock(ModBlocks.SILVER_BLOCK.get());
+        simpleBlock(ModBlocks.SILVER_ORE.get());
+    }
 }
--- src/main/java/com/leaderfirestar/silver/data/client/ModItemModels.java
+++ src/main/java/com/leaderfirestar/silver/data/client/ModItemModels.java
@@ -1,2 +1,24 @@
-package com.leaderfirestar.silver.data.client;public class ModItemModels {
+package com.leaderfirestar.silver.data.client;
+
+import com.leaderfirestar.silver.Main;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelFile;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class ModItemModels extends ItemModelProvider {
+    public ModItemModels(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, Main.MOD_ID, existingFileHelper);
+    }
+    @Override
+    protected void registerModels(){
+        withExistingParent("silver_block", modLoc("block/silver_block"));
+        withExistingParent("silver_ore", modLoc("block/silver_ore"));
+        ModelFile itemGenerated = getExistingFile(mcLoc("item/generated"));
+        builder(itemGenerated, "silver_ingot");
+    }
+
+    private void builder(ModelFile itemGenerated, String name) {
+        getBuilder(name).parent(itemGenerated).texture("layer0", "item/" + name);
+    }
 }
